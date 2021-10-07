10f6f36a01f4166aa51f407ef01d3af816253b63
Oyama <Oyama@100.64.1.73>
2021-10-08 00:12:43 +0900

    first

---
+++ atCoderPractice/src/main/java/abc/PowerSocket.java
@@ -0,0 +1,23 @@
+package abc;
+
+import java.util.Scanner;
+
+public class PowerSocket {
+	public static void main(String [] args) {
+
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int require = sc.nextInt();
+		int plag = 1;
+		int tap = 0;
+						
+		while(true) {
+			if (plag >= require) {
+			System.out.println(tap);
+			return;
+			}
+			plag = plag + a - 1;
+			tap++;
+		}
+	}
+}
---
+++ atCoderPractice/src/main/java/com/example/demo/AtCoderPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtCoderPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtCoderPracticeApplication.class, args);
+	}
+
+}
